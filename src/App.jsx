5a7e3b3d0895eacdbf36e7285e2a33bdce5044dc
yashwingahlawat <yashwingahlawat@gmail.com>
2025-03-20 00:31:09 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+// App.js (or App.jsx)
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<UserLogin/>} />
+        <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
@@ -19,4 +21,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
+// Home.js
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div className='h-full w-full bg-red-400'>
-        <div className='bg-white'>
-          <h2>Get Started with Uber</h2>
-          <button>Continue</button>
-        </div>
+    <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png
+      " alt="" />
+      <div className='bg-white'>
+        <h2>Get Started with Uber</h2>
+        <button>Continue</button>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
