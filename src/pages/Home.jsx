98fbfaa3b37fdcb57f4ff610cf02814a57eb47d6
Yashwin <yashwin.gahlawat@avaloninfosys.com>
2025-03-20 17:59:57 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+    <div className='bg-cover bg-[url(https://images.unsplash.com/photo-1538563188159-070c4db2bc65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRyYWZmaWMlMjBsaWdodHxlbnwwfHwwfHx8MA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
       <img className='w-14 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png
       " alt="" />
       <div className='bg-white px-4 py-4 pb-7'>
