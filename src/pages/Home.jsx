1f1533fb50215740eadedf02d116b3415c127c59
Yashwin <yashwin.gahlawat@avaloninfosys.com>
2025-03-20 17:29:01 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png
+    <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+      <img className='w-14 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png
       " alt="" />
-      <div className='bg-white'>
-        <h2>Get Started with Uber</h2>
-        <button>Continue</button>
+      <div className='bg-white px-4 py-4 pb-7'>
+        <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+        <button className='w-full py-3 bg-black rounded text-white mt-5'>Continue</button>
       </div>
     </div>
   )
