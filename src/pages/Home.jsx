d654e638e40c9702cc0c1e9d3b9cf0a3c5add519
yashwingahlawat <yashwingahlawat@gmail.com>
2025-03-26 23:44:17 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,31 +69,31 @@ const Home = () => {
       <div className='w-full fixed z-10 bottom-0 px-3 py-6 bg-white'>
         <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
         <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
-          <img className='h-12' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="" />
-          <div className='w-1/2'>
+          <img className='h-10' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="" />
+          <div className='ml-2 w-1/2'>
             <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
             <h5 className='font-medium text-sm'>2 mins away</h5>
             <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
           </div>
-          <h2 className='text-xl font-semibold'>₹193.20</h2>
+          <h2 className='text-lg font-semibold'>₹193.20</h2>
         </div>
         <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
-          <img className='h-12' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="" />
-          <div className='w-1/2'>
-            <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
-            <h5 className='font-medium text-sm'>2 mins away</h5>
-            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
+            <h5 className='font-medium text-sm'>3 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable, motorcycle rides</p>
           </div>
-          <h2 className='text-xl font-semibold'>₹193.20</h2>
+          <h2 className='text-lg font-semibold'>₹65.00</h2>
         </div>
         <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
-          <img className='h-12' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="" />
-          <div className='w-1/2'>
-            <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
             <h5 className='font-medium text-sm'>2 mins away</h5>
-            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+            <p className='font-normal text-xs text-gray-600'>Affordable, Auto rides</p>
           </div>
-          <h2 className='text-xl font-semibold'>₹193.20</h2>
+          <h2 className='text-lg font-semibold'>₹113.20</h2>
         </div>
       </div>
     </div>
