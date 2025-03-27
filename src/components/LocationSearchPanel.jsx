2951c960f6c177ba5570ff3d1652aadc769cc299
yashwingahlawat <yashwingahlawat@gmail.com>
2025-03-27 23:25:31 +0530

    update

--- src/components/LocationSearchPanel.jsx
+++ src/components/LocationSearchPanel.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
 const LocationSearchPanel = () => {
+  const locations=["24B, Near Kapoor's Cafe, Coding School, Bhopal","22B, Near Malhotra's Cafe, Coding School, Bhopal"]
   return (
     <div>
+      {
+        locations.map(val=>
+          <div className='items-start gap-4 p-3 border-2 border-gray-50 rounded-xl active:border-black flex justify-start my-2'>
+        <h2 className='bg-[#eee] h-10 flex justify-center items-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>{val}</h4>
+      </div>
+        )
+      }
       {/* this is just a sample data. */}
-      <div className='items-start gap-4 flex justify-start my-4'>
+      {/* <div className='items-start gap-4 p-3 border-2 border-gray-50 rounded-xl active:border-black flex justify-start my-2'>
         <h2 className='bg-[#eee] h-10 flex justify-center items-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's Cafe, Coding School, Bhopal</h4>
       </div>
-      <div className='items-start gap-4 flex justify-start my-4'>
+      <div className='items-start gap-4 p-3 border-2 border-gray-50 rounded-xl active:border-black flex justify-start my-2'>
         <h2 className='bg-[#eee] h-10 flex justify-center items-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
         <h4 className='font-medium'>24B, Near Kapoor's Cafe, Coding School, Bhopal</h4>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,9 +66,9 @@ const Home = () => {
           <LocationSearchPanel/>
         </div>
       </div>
-      <div className='w-full fixed z-10 bottom-0 px-3 py-6 bg-white'>
+      <div className='w-full fixed z-10 bottom-0 translate-y-full px-3 py-8 bg-white'>
         <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
-        <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
+        <div className='w-full border-2 active:border-black rounded-xl flex items-center justify-between p-3 mb-3'>
           <img className='h-10' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="" />
           <div className='ml-2 w-1/2'>
             <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
@@ -77,7 +77,7 @@ const Home = () => {
           </div>
           <h2 className='text-lg font-semibold'>₹193.20</h2>
         </div>
-        <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
+        <div className='w-full border-2 active:border-black rounded-xl flex items-center justify-between p-3 mb-3'>
           <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
           <div className='ml-2 w-1/2'>
             <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
@@ -86,7 +86,7 @@ const Home = () => {
           </div>
           <h2 className='text-lg font-semibold'>₹65.00</h2>
         </div>
-        <div className='w-full border-2 border-black rounded-xl flex items-center justify-between p-3 mb-3'>
+        <div className='w-full border-2 active:border-black rounded-xl flex items-center justify-between p-3 mb-3'>
           <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
           <div className='ml-2 w-1/2'>
             <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
